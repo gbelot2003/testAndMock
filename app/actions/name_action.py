086f26daf8cd9cef0d209f897550f542a5e15bba
Gerardo Belot <gbelot2003@hotmail.com>
2024-10-15 11:41:16 -0600

    :construction: agregando sesssion

--- app/actions/name_action.py
+++ app/actions/name_action.py
@@ -4,19 +4,23 @@ from ..repos.contact_repo import ContactRepo
 import logging
 
 class NameAction:
-    def __init__(self, contacto, prompt):
+    def __init__(self, db_session, contacto, prompt):
         self.contacto = contacto
         self.prompt = prompt
+        self.db_session = db_session
 
     def process_name(self):
         message = None
         # Si el contacto tiene un nombre, usarlo en la conversación
         if self.contacto.nombre:
+            print(f"El usuario se llama y llamalo {self.contacto.nombre}.")
             message = {"role": "assistant", "content": f"El usuario se llama y llamalo {self.contacto.nombre}."}
         else:
-            extraerNombre = NombreExtractor().extraer_nombre(self.prompt)            # grabar nombre en base de datos
+            extraerNombre = NombreExtractor().extraer_nombre(self.prompt)  
+            print(f"El contacto es {self.contacto}")
+            # grabar nombre en base de datos
             if extraerNombre:
-                ContactRepo().actualizar_contacto(self.contacto.id, nombre=extraerNombre)
+                ContactRepo().actualizar_contacto(self.db_session, self.contacto.id, nombre=extraerNombre)
                 message = {"role": "assistant", "content": f"El usuario se llama y llamalo {extraerNombre}."}
             else:
                 message = {"role": "system", "content": "pregunte al usuario en que le podemos ayudar y cual es su nombre para mejor servicio."}
